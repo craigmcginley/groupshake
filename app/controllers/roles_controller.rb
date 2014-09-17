1b8aa0bc7f5877a48a4160c41537c0bc9d3f7230
craigmcginley <craigmcginley@gmail.com>
2014-09-17 17:45:00 -0400

    added column sorting and pagination

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,6 +1,18 @@
 class RolesController < ApplicationController
+  helper_method :sort_column, :sort_direction
+
   def index
     @play = Play.find(params[:play_id])
-    @roles = Role.where(play: @play)
+    @roles = Role.where(play: @play).paginate(page: params[:page], per_page: 10).order(sort_column + " " + sort_direction)
+  end
+
+  private
+
+  def sort_column
+    Role.column_names.include?(params[:sort]) ? params[:sort] : "name"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 end
