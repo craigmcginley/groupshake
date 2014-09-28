e82e4b05ddd3bcb7e9fbd8e1343e37e67b7d4e64
Catherine Powell <catherine@abakas.com>
2014-09-28 11:14:26 -0400

    Note on validations

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,6 +1,12 @@
 class Role < ActiveRecord::Base
   belongs_to :play
 
+  # Although this is perfectly legal according to the spec
+  # we usually don't do it because validations tend to 
+  # change. If you put them on separate lines then it
+  # makes diffs smaller and makes their intent more clear
+  # because it shows the actual change in valdation
+  # rather than line manipulation/overhead.
   validates_presence_of :name, :play
   validates_uniqueness_of :name, scope: :play_id
 end
